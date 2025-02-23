a9dfa9277cf8617b1516141739969ef1da93ed28
AnasAlsadany <148365426+AnasAlsadany@users.noreply.github.com>
2025-02-23 14:42:11 +0200

    fix status component

--- src/app/@shared/components/shared-status/shared-status.component.ts
+++ src/app/@shared/components/shared-status/shared-status.component.ts
@@ -1,18 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-shared-status',
   standalone: true,
   imports: [
-    CommonModule,
-    TranslateModule
+    CommonModule
   ],
   templateUrl: './shared-status.component.html',
   styleUrl: './shared-status.component.scss'
 })
 export class SharedStatusComponent {
   @Input() statusConfig:any;
-  @Input() data:any
+  @Input() data:any;
+  @Input() label?:string;
 }
